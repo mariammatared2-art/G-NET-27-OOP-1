748e0a63a29e4620bbb852b5bbb958cf0c3a07c0
mariam matared <mariammatared2@gmail.com>
2026-02-18 19:22:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Net.Cache;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Part 1
+            // Question 1
+            //Student student1  = new Student();
+            //student1.Age = 10;
+            //Student student2 = student1 ;
+            //student2.Age = 50;
+            //Console.WriteLine(student1.Age);
+            //Console.WriteLine(student2.Age);
+
+            //Point p1 = new Point();
+            //Point y = p1;
+            //p1.x = 5;
+            //y.x = 10;
+            //Console.WriteLine(p1.x);
+            //Console.WriteLine(y.x);
+            //-------------------------------------------------------------------------------------------
+            // Question 2
+
+            //Public: The member is accessible from any other code .
+            //Private: The member is accessible only within the body of the class or struct.
+
+            //public class User
+            //{
+            //private string password; 
+            //public string UserName;  
+
+            //public void SetPassword(string pass)
+            // {
+            //    password = pass; 
+            // }
+            //}
+            //-----------------------------------------------------------------------------------------------
+            // Question 3
+            //Create: Open Visual Studio → Create a new project → Select Class Library template.
+            //Code: Write your classes and logic, then Build the project(creates a .dll file).
+            //Reference: Right - click Dependencies in your main project → Add Project Reference → Select your library.
+            //Consume: Add the using LibraryName; directive at the top of your code file to access its classes.
+            //--------------------------------------------------------------------------------------------------
+            //Question 4
+            // A Class Library is a separate projectt that contains reusable classes but has no main method and cannot run on its own .
+            // We use Class Library : Reusability , Maintainability , Teamwork , Organization 
+
+
+            #endregion
+
+
+
+        }
+    }
+}
